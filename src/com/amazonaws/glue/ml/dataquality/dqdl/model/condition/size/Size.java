38bfc2174e8efe553f1a170b3dd35ca4d6f90a80
Sam Pomerantz <sam@sampomerantz.me>
2024-10-29 05:42:42 -0400

    Adapt ETL FileFreshness to Parsing Changes + Fix Bug with IN / NOT_IN, Tweak Size

--- src/com/amazonaws/glue/ml/dataquality/dqdl/model/condition/size/Size.java
+++ src/com/amazonaws/glue/ml/dataquality/dqdl/model/condition/size/Size.java
@@ -10,20 +10,45 @@
 
 package com.amazonaws.glue.ml.dataquality.dqdl.model.condition.size;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.io.Serializable;
 
-@AllArgsConstructor
 @Getter
 @EqualsAndHashCode
-public class Size implements Serializable {
+public class Size implements Serializable, Comparable<Size> {
     private final Integer amount;
     private final SizeUnit unit;
+    private final Long bytes;
+
+    public Size(final Integer amount, final SizeUnit unit) {
+        this.amount = amount;
+        this.unit = unit;
+        this.bytes = convertBytes(amount, unit);
+    }
 
     public String getFormattedSize() {
         return String.format("%s %s", amount, unit.name().toUpperCase());
+    }
+
+    private Long convertBytes(Integer bytes, SizeUnit unit) {
+        switch (unit) {
+            case KB:
+                return bytes * 1024L;
+            case MB:
+                return bytes * 1024L * 1024L;
+            case GB:
+                return bytes * 1024L * 1024L * 1024L;
+            case TB:
+                return bytes * 1024L * 1024L * 1024L * 1024L;
+            default:
+                return Long.valueOf(bytes);
+        }
+    }
+
+    @Override
+    public int compareTo(Size other) {
+        return Long.compare(this.getBytes(), other.getBytes());
     }
 }
